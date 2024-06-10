deda7397f056f1280dc4afea1e5fc162476613e9
Eduardo Teixeira <eduardo.apteixeira00@gmail.com>
2024-06-10 11:44:56 -0300

    feat: types and css implementation

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="h-screen flex flex-col flex-nowrap justify-between">
+    <div>
+      <Header />
+      <UContainer class="my-4 overflow-auto overflow-x-hidden">
+        <slot></slot>
+      </UContainer>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
